1e70a30f0448c7d04cdee3b1fb2df0aadd493ca8
Soniaman0 <148853251+Soniaman0@users.noreply.github.com>
2024-04-10 14:49:14 +0530

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.Amansoni.Employeemanagementproject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
